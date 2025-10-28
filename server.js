d6d1cea6def0a01b246c256d6ab0d98bcf2ff4d9
Kasper <106410740+kasperwtrcolor@users.noreply.github.com>
2025-10-28 22:40:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post("/api/handleTweet", async (req, res) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "x-devbase-secret": process.env.x-devbase-secret
+        "x-devbase-secret": process.env.DEVBASE_SECRET
       },
       body: JSON.stringify({
         args: [sender_handle, recipient, amount]
